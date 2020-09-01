5ddc7b078bd1914ca3e2d056690320a12b506265
Taylor <taylor@coupang.com>
2020-09-02 01:03:02 +0900

    CSS 수정 / 명언 가져오는 API 수정

--- app.py
+++ app.py
@@ -1,23 +1,30 @@
-from flask import Flask, render_template, jsonify, request
-from pymongo import MongoClient
-import random
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
-db = client.dbsparta
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/showCat', methods=['GET'])
-def read_articles():
-    cats = list(db.cats.find({}, {'_id': False}))
-    cat = random.choice(cats)
-    return jsonify({'result': 'success', 'cat': cat})
+@app.route('/mypage')
+def my_page():
+    return 'This is My Page!'
+
+@app.route('/content')
+def get_content():
+    # 1. DB 에서 content 1개 Random 으로 가져오기
+    # 2. client 에 JSON 형태로 내려주기
+    # {
+    #   content = "오늘 최선이 내 인생의 최선이다."
+    # }
+
+    randomContent = {
+      "content": "오늘 최선이 내 인생의 최선이다."
+    }
+
+    return jsonify(randomContent)
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
-
-    # app > div > div:nth-child(3) > div:nth-child(3) > div > div:nth-child(1) > div > div > div:nth-child(1) > div:nth-child(1) > figure > div > div._3A74U > div > div > a > div > img
